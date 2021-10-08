32d15e545160e612914f4d81061e049079a7c330
Me <johnpuliatch@gmail.com>
2021-10-08 11:46:36 -1000

    Create boot project, add JPA Project dependencies

---
+++ WalkingTracker/src/main/java/com/skilldistillery/walkingtracker/WalkingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.walkingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingTrackerApplication.class, args);
+	}
+
+}
---
+++ WalkingTracker/src/test/java/com/skilldistillery/walkingtracker/WalkingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.walkingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
