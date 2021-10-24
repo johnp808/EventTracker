0b396ff5954778f143dc42786e8241bd48fe8d42
Me <johnpuliatch@gmail.com>
2021-10-24 12:00:33 -1000

    fixed weird issue with add form

--- ngWalkingTracker/src/app/models/contestant.ts
+++ ngWalkingTracker/src/app/models/contestant.ts
@@ -1,9 +1,39 @@
 export class Contestant {
   id: number;
   name: string;
+  gender: string;
+  age: number;
+  contestantImage: string;
+  representingCity: string;
+  representingState: string;
+  eventName: string;
+  eventDate: string;
+  finishTime: string;
+  place: number;
 
-  constructor(id: number = 0, name: string = '') {
+  constructor(id: number = 0,
+    name: string = '',
+    gender: string = '',
+    age: number = 0,
+    contestantImage: string = '',
+    representingCity: string = '',
+    representingState: string = '',
+    eventName: string = '',
+    eventDate: string = '',
+    finishTime: string = '',
+    place: number = 0
+     ) {
+
     this.id = id;
     this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.contestantImage = contestantImage;
+    this.representingCity = representingCity;
+    this.representingState = representingState;
+    this.eventName = eventName;
+    this.eventDate = eventDate;
+    this.finishTime = finishTime;
+    this.place = place;
   }
 }
